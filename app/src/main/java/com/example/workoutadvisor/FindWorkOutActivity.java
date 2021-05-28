8bdd5d3428cd7e787eaa212b8543ae5778ed34ab
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-05-28 16:43:22 +0530

    added change logic

--- app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
+++ app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
@@ -1,14 +1,25 @@
 package com.example.workoutadvisor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindWorkOutActivity extends AppCompatActivity {
+public class FindWorkOutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindWorkout(View view) {
+        TextView workouts = (TextView) findViewById(R.id.workout);
+        Spinner workouttype = (Spinner) findViewById(R.id.workouttype);
+
+        String workout = String.valueOf(workouttype.getSelectedItem());
+
+        workouts.setText(workout);
     }
 }
