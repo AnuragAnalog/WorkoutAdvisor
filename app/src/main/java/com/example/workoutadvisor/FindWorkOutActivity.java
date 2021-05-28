7528c166aa89ddca50711d61b3a5740bc032e97b
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-05-28 14:55:23 +0530

    Added Button, Text, Dropdown

---
+++ app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
@@ -0,0 +1,14 @@
+package com.example.workoutadvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindWorkOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
