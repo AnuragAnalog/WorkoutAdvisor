1e41b37c3c6821cb0367be2eb694c514e0c4a3eb
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-05-29 19:20:21 +0530

    added some new features
    Included many workouts, current time and date, added home vs gym toggle button.

--- app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
+++ app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
@@ -3,19 +3,50 @@ package com.example.workoutadvisor;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class FindWorkOutActivity extends Activity {
 
+    boolean isGym;
     private WorkoutExpert expert = new WorkoutExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setting the date
+        TextView date = (TextView) findViewById(R.id.textDate);
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/mm/yyyy");
+        String currentDate = sdf1.format(new Date());
+        date.setText(currentDate);
+
+        // Setting the time
+        TextView time = (TextView) findViewById(R.id.textTime);
+        SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss");
+        String currentTime = sdf2.format(new Date());
+        time.setText(currentTime);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.switchButton);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    isGym = true;
+                }
+                else {
+                    isGym = false;
+                }
+            }
+        });
     }
 
     public void onClickFindWorkout(View view) {
@@ -26,13 +57,14 @@ public class FindWorkOutActivity extends Activity {
 
 //        workouts.setText(workout);
 
-        List<String> workoutList = expert.getworkout(workout);
+        List<String> workoutList = expert.getworkout(workout, isGym);
 
         StringBuilder workoutFormatted = new StringBuilder();
 
         for (String work : workoutList) {
             workoutFormatted.append(work).append("\n");
         }
+
         workouts.setText(workoutFormatted);
     }
 }
--- app/src/main/java/com/example/workoutadvisor/WorkoutExpert.java
+++ app/src/main/java/com/example/workoutadvisor/WorkoutExpert.java
@@ -4,33 +4,83 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WorkoutExpert {
-    List <String> getworkout(String workouttype)
+    List <String> getworkout(String workouttype, boolean isGym)
     {
         List <String> workout = new ArrayList<>();
 
-        if (workouttype.equals("Chest"))
-        {
-            workout.add("Bench Press");
-            workout.add("Cable Flys");
+        if (workouttype.equals("Chest")) {
+            if (isGym) {
+                workout.add("Cable Flys");
+                workout.add("Bench Press");
+                workout.add("Cable crossover");
+                workout.add("Chest press");
+            }
+            else {
+                workout.add("Incline Push ups");
+                workout.add("Decline Push ups");
+                workout.add("Knee Push ups");
+                workout.add("Wide Push ups");
+                workout.add("Box Push ups");
+            }
         }
         else if (workouttype.equals("Triceps"))
         {
-            workout.add("Tricep Extension");
-            workout.add("Tricep Pushdown");
+            if (isGym) {
+                workout.add("Diamond Push-Ups");
+                workout.add("Bench Dip");
+                workout.add("One-Arm Overhead Extension");
+            }
+            else {
+                workout.add("Triceps Bow");
+                workout.add("Power Tricep Extension");
+                workout.add("Tricep Dips");
+            }
         }
         else if (workouttype.equals("Shoulders"))
         {
-            workout.add("Shoulder Press");
-            workout.add("Shoulder Extension");
+            if (isGym) {
+                workout.add("Barbell Overhead Shoulder Press");
+                workout.add("Seated Dumbbell Shoulder Press");
+                workout.add("Front Raise");
+                workout.add("Reverse Pec Deck Fly");
+            }
+            else {
+                workout.add("Arm Raises");
+                workout.add("Side Arm raise");
+                workout.add("Rhomboid pulls");
+                workout.add("Arm Scissors");
+            }
         }
         else if (workouttype.equals("Biceps"))
         {
-            workout.add("Bicep Curls");
+            if (isGym) {
+                workout.add("Alternating Incline Dumbbell Curl");
+                workout.add("Seated Alternating Hammer Curl");
+                workout.add("Standing Reverse Barbell Curl");
+                workout.add("Seated Alternating Dumbbell Curl");
+            }
+            else {
+                workout.add("One Arm Push-Ups");
+                workout.add("Side Plank");
+                workout.add("Plank Up-Down");
+                workout.add("Curls");
+            }
         }
         else if (workouttype.equals("Legs"))
         {
-            workout.add("Leg Curl");
-            workout.add("Leg Extension");
+            if (isGym) {
+                workout.add("Leg Curl");
+                workout.add("Leg Extension");
+                workout.add("Glute Bridge");
+                workout.add("Calf Raise");
+            }
+            else {
+                workout.add("Backward Lunge");
+                workout.add("Side Hop");
+                workout.add("Squats");
+                workout.add("Side Lying Leg Lift Left");
+                workout.add("Side Lying Leg Lift Right");
+            }
         }
 
         return workout;
