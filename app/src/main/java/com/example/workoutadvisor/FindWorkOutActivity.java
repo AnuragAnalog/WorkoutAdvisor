aa1b3b43c680fd92384be71fe544d487a8df041a
AnuragAnalog <anurag.peddi1998@gmail.com>
2021-05-29 13:08:38 +0530

    added new class and logic to display different workouts

--- app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
+++ app/src/main/java/com/example/workoutadvisor/FindWorkOutActivity.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class FindWorkOutActivity extends Activity {
+
+    private WorkoutExpert expert = new WorkoutExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class FindWorkOutActivity extends Activity {
 
         String workout = String.valueOf(workouttype.getSelectedItem());
 
-        workouts.setText(workout);
+//        workouts.setText(workout);
+
+        List<String> workoutList = expert.getworkout(workout);
+
+        StringBuilder workoutFormatted = new StringBuilder();
+
+        for (String work : workoutList) {
+            workoutFormatted.append(work).append("\n");
+        }
+        workouts.setText(workoutFormatted);
     }
 }
---
+++ app/src/main/java/com/example/workoutadvisor/WorkoutExpert.java
@@ -0,0 +1,38 @@
+package com.example.workoutadvisor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkoutExpert {
+    List <String> getworkout(String workouttype)
+    {
+        List <String> workout = new ArrayList<>();
+
+        if (workouttype.equals("Chest"))
+        {
+            workout.add("Bench Press");
+            workout.add("Cable Flys");
+        }
+        else if (workouttype.equals("Triceps"))
+        {
+            workout.add("Tricep Extension");
+            workout.add("Tricep Pushdown");
+        }
+        else if (workouttype.equals("Shoulder"))
+        {
+            workout.add("Shoulder Press");
+            workout.add("Shoulder Extension");
+        }
+        else if (workouttype.equals("Biceps"))
+        {
+            workout.add("Bicep Curls");
+        }
+        else if (workouttype.equals("Legs"))
+        {
+            workout.add("Leg Curl");
+            workout.add("Leg Extension");
+        }
+
+        return workout;
+    }
+}
